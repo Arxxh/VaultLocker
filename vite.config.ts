cdd0c178c5c5b6aa44152c61ac7899b187b711ae
Diego Silva <diego23montes@outlook.com>
2025-11-16 00:03:46 -0600

    paquetes de desarrollo tailwind, react, vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,19 @@ import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        popup: path.resolve(__dirname, 'src/popup/index.html'),
-        dashboard: path.resolve(__dirname, 'src/dashboard/index.html'),
-        background: path.resolve(__dirname, 'src/background/index.ts'),
-        content: path.resolve(__dirname, 'src/content/detectForms.ts'),
-      },
-    },
-  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    cssCodeSplit: false, // fuerza que se inyecte 1 solo CSS
   },
 });
